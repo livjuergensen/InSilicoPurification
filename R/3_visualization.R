95c1d437e6f982a4eb6d1ef2d2bb4888bae28270
livjuergensen <liv.juergensen@charite.de>
2025-04-08 20:44:06 +0200

    Update 3_visualization.R

--- R/3_visualization.R
+++ R/3_visualization.R
@@ -8,7 +8,7 @@ dir.create("plots")
 plot_mcf <- TRUE
 plot_mc  <- TRUE
 
-colors <- readRDS(file.path("data", "colors_pur.rds"))
+colors <- readRDS(file.path("data", "colors_pur.RDS"))
 
 # General plotting function
 plot_prob <- function(prob, color_vector, output_suffix) {
